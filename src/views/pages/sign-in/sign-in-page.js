b1cc348040927f6a9ab31ba57cb8b54eae4e9534
alexchitu <alexchitu1991@outlook.com>
2018-12-11 16:10:02 +0200

    fixed auth issue

--- src/views/pages/sign-in/sign-in-page.js
+++ src/views/pages/sign-in/sign-in-page.js
@@ -13,9 +13,9 @@ const SignInPage = ({signInWithGithub, signInWithGoogle, signInWithTwitter}) => 
     <div className="g-row sign-in">
       <div className="g-col">
         <h1 className="sign-in__heading">Sign in</h1>
-        <Button className="sign-in__button" onClick={signInWithGithub}>GitHub</Button>
+        {/* <Button className="sign-in__button" onClick={signInWithGithub}>GitHub</Button> */}
         <Button className="sign-in__button" onClick={signInWithGoogle}>Google</Button>
-        <Button className="sign-in__button" onClick={signInWithTwitter}>Twitter</Button>
+        {/* <Button className="sign-in__button" onClick={signInWithTwitter}>Twitter</Button> */}
       </div>
     </div>
   );
